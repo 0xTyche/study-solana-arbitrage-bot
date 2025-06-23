3dadcc81eee7d8f33389b9535224d125348b041b
0xTyche <22494749@qq.com>
2025-06-23 12:28:34 +0000

    初始化：初始化池子方法（内容未写）

---
+++ solana-arbitrage-bot/refresh.rs
@@ -0,0 +1,32 @@
+use crate::constants::sol_mint;
+use crate::dex::dlmm::{constants::dlmm_program_id, dlmm_info::DlmmInfo};
+use crate::dex::pump::{pump_fee_wallet, pump_program_id, PumpAmmInfo};
+use crate::dex::raydium::{
+    get_tick_array_pubkeys, raydium_clmm_program_id, raydium_cp_program_id, raydium_program_id,
+    PoolState, RaydiumAmmInfo, RaydiumCpAmmInfo,
+};
+use crate::dex::whirlpool::{
+    constants::whirlpool_program_id, state::Whirlpool, update_tick_array_accounts_for_onchain,
+};
+use crate::pools::*;
+use solana_client::rpc_client::RpcClient;
+use solana_program::pubkey::Pubkey;
+use spl_associated_token_account;
+use std::str::FromStr;
+use std::sync::Arc;
+use tracing::{error, info};
+
+// 初始化池子的数据
+pub async fn initialize_pool_data(
+    mint: &str,
+    wallet_account: &str,
+    raydium_pools: Option<&Vec<String>>,
+    raydium_cp_pools: Option<&Vec<String>>,
+    pump_pools: Option<&Vec<String>>,
+    dlmm_pools: Option<&Vec<String>>,
+    whirlpool_pools: Option<&Vec<String>>,
+    raydium_clmm_pools: Option<&Vec<String>>,
+    rpc_client: Arc<RpcClient>,
+) -> anyhow::Result<MintPoolData> {
+    
+}
